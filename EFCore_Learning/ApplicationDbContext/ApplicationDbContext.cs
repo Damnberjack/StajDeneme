dab8fb1ad18b46a72637163e9d6f30e484986638
Coders Clup <hi@codersclub.co>
2023-08-18 17:17:04 +0300

    ilk EFCore_Learning task'ı oluşturuldu

---
+++ EFCore_Learning/ApplicationDbContext/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EFCore_Learning.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore_Learning.ApplicationDbContext;
+
+public class AppbContext:DbContext
+{
+    public AppbContext(DbContextOptions<AppbContext> options) : base(options)
+    {
+
+    }
+    public DbSet<Product> Products { get; set; }
+}
---
+++ EFCore_Learning/Controllers/EFCoreController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EFCore_Learning.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EFCoreController : ControllerBase
+    {
+
+    }
+}
